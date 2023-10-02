dc3e1a5c773b44a57ee8b78ac95a8b5f3c46c9c5
Abigael chemtai <abigaelchemtai8@gmail.com>
2023-10-02 06:19:05 -0700

    header

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -7,7 +7,7 @@ import sys
 if __name__ == "__main__":
     url = sys.argv[1]
 
-    res  = requests.get(url)
+    response  = requests.get(url)
     body_of_response = response.text
 
     if response.status_code >= 400:
