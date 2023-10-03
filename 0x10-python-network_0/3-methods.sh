5c397d31e1fba8afbde15c6cc90c66cd42b848aa
Abigaelchemtai <abigaelchemtai8@gmail.com>
2023-10-03 03:13:24 -0700

    3-METHODS

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# Bash script that takes in a URL
-# Displays all HTTP methods the server will accept
-curl - sI "$1" | grep "Allow" | cut - d " " - f 2-
+# Bash script that takes in a URL Displays all HTTP methods the server will accept
+curl -sI "$1" | grep "Allow" | cut -d " " -f 2-
