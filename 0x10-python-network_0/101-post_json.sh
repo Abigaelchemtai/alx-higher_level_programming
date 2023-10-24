850ec9a19ebbd5bb31b67f5ecc771c40d967efbd
Abigaelchemtai <118007085+Abigaelchemtai@users.noreply.github.com>
2023-10-24 12:37:12 +0300

    Delete 0x10-python-network_0 directory

--- 0x10-python-network_0/101-post_json.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# Bash script that sends a json post request to a URL passed as the first argument and display the body of the response
-curl -s -H "Content-Type: application/json" -d "$(cat "$2")" "$1"
